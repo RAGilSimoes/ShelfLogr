e35e6d63e420bcac7eb36a34d829d9194fe156a7
Ricardo André Simões <ricardo.andre.simoes@gmail.com>
2026-08-05 11:32:31 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ShelfLogr',
+  webDir: 'dist'
+};
+
+export default config;
