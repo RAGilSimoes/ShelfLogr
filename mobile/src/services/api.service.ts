ef753339f3162391a77f007d166ca09eb0ea3d52
Ricardo André Simões <ricardo.andre.simoes@gmail.com>
2026-08-09 16:06:11 +0100

    interceptor for when token expires

--- mobile/src/services/api.service.ts
+++ mobile/src/services/api.service.ts
@@ -7,7 +7,7 @@ import type {
   InternalAxiosRequestConfig,
 } from 'axios';
 
-import { checkToken } from './auth.service';
+import { checkToken, removeToken, setToken } from './auth.service';
 
 const api: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_URL,
@@ -26,7 +26,33 @@ api.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
 
 api.interceptors.response.use(
   (response: AxiosResponse) => response,
-  (error) => Promise.reject(error),
+  async (error) => {
+    if (error.response.status === 401 && !error.config._retry) {
+      const request = error.config;
+      error.config._retry = true;
+
+      try {
+        const result = await axios.get(
+          `${import.meta.env.VITE_BACKEND_URL}/refresh-token`,
+          {
+            headers: { Authorization: request.headers['Authorization'] },
+          },
+        );
+
+        if (result.status === 200) {
+          const token = result.data.token;
+          setToken(token);
+          request.headers['Authorization'] = `Bearer ${token}`;
+
+          return api(request);
+        }
+      } catch (error) {
+        removeToken();
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  },
 );
 
 export default api;
