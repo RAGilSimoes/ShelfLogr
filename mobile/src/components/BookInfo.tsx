0d722dfc5e9b9b3d809042e7e4e3a42822938ec6
Ricardo André Simões <ricardo.andre.simoes@gmail.com>
2026-08-13 16:03:01 +0100

    added isbn alert when reading barcode, if user wants to edit the isbn to search

--- mobile/src/components/BookInfo.tsx
+++ mobile/src/components/BookInfo.tsx
@@ -14,9 +14,12 @@ import { bookInfo, bookCover } from '@shelflogr/shared';
 
 import styles from './BookInfo.module.css';
 
-const BookInfo: React.FC<{ bookInfo: bookInfo }> = ({ bookInfo }) => {
+const BookInfo: React.FC<{ bookInfo: bookInfo; detailed: boolean }> = ({
+  bookInfo,
+  detailed,
+}) => {
   return (
-    <IonCard>
+    <IonCard className={styles.card}>
       <IonGrid>
         <IonRow>
           <IonCol size="4">
@@ -52,66 +55,75 @@ const BookInfo: React.FC<{ bookInfo: bookInfo }> = ({ bookInfo }) => {
           </IonCol>
         </IonRow>
 
-        <hr className={styles.hr} />
+        {detailed && (
+          <>
+            <hr className={styles.hr} />
 
-        <IonRow>
-          <IonCol size="12">
-            <IonCardContent
-              className={`ion-no-padding ${styles.secondaryInfo}`}
-            >
-              <h3 className={styles.sinopse}>Sinopse</h3>
-              <p className={styles.description}>
-                {bookInfo.description
-                  ? bookInfo.description
-                  : 'No description found.'}
-              </p>
+            <IonRow>
+              <IonCol size="12">
+                <IonCardContent
+                  className={`ion-no-padding ${styles.secondaryInfo}`}
+                >
+                  <h3 className={styles.sinopse}>
+                    <strong>Sinopse:</strong>
+                  </h3>
+                  <p className={styles.description}>
+                    {bookInfo.description
+                      ? bookInfo.description
+                      : 'No description found.'}
+                  </p>
 
-              <div className={styles.categories}>
-                <p className={styles.mainCategory}>
-                  <strong>Main Category:</strong>
-                </p>
-                <IonChip color="primary">
-                  {bookInfo.mainCategory
-                    ? bookInfo.mainCategory
-                    : 'No Main Category found.'}
-                </IonChip>
-
-                {bookInfo.categories && bookInfo.categories.length !== 0 && (
-                  <>
-                    <p className={styles.secondaryCategoriesTitle}>
-                      <strong>Secondary Categories:</strong>
+                  <div className={styles.categories}>
+                    <p className={styles.mainCategory}>
+                      <strong>Main Category:</strong>
                     </p>
+                    <IonChip color="primary">
+                      {bookInfo.mainCategory
+                        ? bookInfo.mainCategory
+                        : 'No Main Category found.'}
+                    </IonChip>
 
-                    <div className={styles.secondaryCategoriesDiv}>
-                      {bookInfo.categories.map((category, index) => {
-                        return (
-                          <IonChip
-                            key={index}
-                            color="secondary"
-                            className={styles.secondaryCategoriesChip}
-                          >
-                            {category}
-                          </IonChip>
-                        );
-                      })}
-                    </div>
-                  </>
-                )}
-              </div>
+                    {bookInfo.categories &&
+                      bookInfo.categories.length !== 0 && (
+                        <>
+                          <p className={styles.secondaryCategoriesTitle}>
+                            <strong>Secondary Categories:</strong>
+                          </p>
 
-              <div className={styles.footerInfo}>
-                <span>
-                  <strong>Pages:</strong>{' '}
-                  {bookInfo.pageCount ? bookInfo.pageCount : 'N/A'}
-                </span>
-                <span>
-                  <strong>Language:</strong>{' '}
-                  {bookInfo.language ? bookInfo.language.toUpperCase() : 'N/A'}
-                </span>
-              </div>
-            </IonCardContent>
-          </IonCol>
-        </IonRow>
+                          <div className={styles.secondaryCategoriesDiv}>
+                            {bookInfo.categories.map((category, index) => {
+                              return (
+                                <IonChip
+                                  key={index}
+                                  color="secondary"
+                                  className={styles.secondaryCategoriesChip}
+                                >
+                                  {category}
+                                </IonChip>
+                              );
+                            })}
+                          </div>
+                        </>
+                      )}
+                  </div>
+
+                  <div className={styles.footerInfo}>
+                    <span>
+                      <strong>Pages:</strong>{' '}
+                      {bookInfo.pageCount ? bookInfo.pageCount : 'N/A'}
+                    </span>
+                    <span>
+                      <strong>Language:</strong>{' '}
+                      {bookInfo.language
+                        ? bookInfo.language.toUpperCase()
+                        : 'N/A'}
+                    </span>
+                  </div>
+                </IonCardContent>
+              </IonCol>
+            </IonRow>
+          </>
+        )}
       </IonGrid>
     </IonCard>
   );
--- mobile/src/pages/Add.tsx
+++ mobile/src/pages/Add.tsx
@@ -15,6 +15,7 @@ import {
   IonCardTitle,
   useIonViewWillLeave,
   IonIcon,
+  IonAlert,
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 
@@ -44,6 +45,9 @@ const Add: React.FC = () => {
   const [displayErrorMessage, setDisplayErrorMessage] =
     useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isAlertOpen, setIsAlertOpen] = useState<boolean>(false);
+
+  const [tempIsbn, setTempIsbn] = useState<number>(0);
 
   const checkPermissions = async () => {
     const { camera } = await BarcodeScanner.checkPermissions();
@@ -70,6 +74,7 @@ const Add: React.FC = () => {
   const lerBarcode = async () => {
     setErrorMessage('');
     setDisplayErrorMessage(false);
+    setTempIsbn(0);
 
     let cameraPermission = await checkPermissions();
 
@@ -80,28 +85,25 @@ const Add: React.FC = () => {
     if (cameraPermission === 'granted') {
       try {
         const barcodes = await scan();
-        setIsLoading(true);
+
         if (barcodes.length > 0 && barcodes[0].valueType === 'ISBN') {
           const isbn = barcodes[0].displayValue;
 
-          const response = await api.get(`/get-book-info/${isbn}`);
+          setTempIsbn(Number(isbn));
 
-          if (response.status === 200) {
-            const responseBookInfo: bookInfo = response.data;
-            setBookInfo(responseBookInfo);
-            setIsLoading(false);
-          }
+          setIsAlertOpen(true);
         } else {
           setErrorMessage("Barcode doesn't match a book");
+
           setDisplayErrorMessage(true);
         }
       } catch (error) {
-        if (axios.isAxiosError(error)) {
-          setDisplayErrorMessage(true);
-          const serverMessage =
-            error.response?.data?.error || 'Server communication error.';
-          setErrorMessage(serverMessage);
+        if (error instanceof Error) {
+          setErrorMessage(error.message);
+        } else {
+          setErrorMessage('Error scanning barcode');
         }
+        setDisplayErrorMessage(true);
       } finally {
         BarcodeScanner.stopScan();
         setIsLoading(false);
@@ -115,6 +117,29 @@ const Add: React.FC = () => {
   useIonViewWillLeave(() => {
     setBookInfo(undefined);
   });
+
+  const fetchBookInfo = async (isbn: number) => {
+    setIsLoading(true);
+    try {
+      const response = await api.get(`/get-book-info/${isbn}`);
+
+      if (response.status === 200) {
+        const responseBookInfo: bookInfo = response.data;
+        setBookInfo(responseBookInfo);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setDisplayErrorMessage(true);
+        const serverMessage =
+          error.response?.data?.error || 'Server communication error.';
+        setErrorMessage(serverMessage);
+      }
+    } finally {
+      setIsLoading(false);
+      setIsAlertOpen(false);
+    }
+  };
 
   return (
     <IonPage>
@@ -131,29 +156,73 @@ const Add: React.FC = () => {
           className={styles.customToast}
           position="top"
         ></IonToast>
+
+        <IonAlert
+          isOpen={isAlertOpen}
+          header="ISBN Detected"
+          message="Confirm if the displayed ISBN matches the one from the book"
+          cssClass="custom-isbn-alert"
+          inputs={[
+            {
+              type: 'number',
+              name: 'isbnField',
+              value: `${tempIsbn}`,
+              placeholder: `${tempIsbn}`,
+              cssClass: 'alert-isbn-input',
+              attributes: {
+                minLength: 13,
+                maxlength: 13,
+              },
+            },
+          ]}
+          buttons={[
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              cssClass: 'alert-cancel-button',
+            },
+            {
+              text: 'Get Info',
+              role: 'confirm',
+              cssClass: 'alert-confirm-button',
+              handler: (alertData) => {
+                const finalIsbn = Number(alertData.isbnField);
+                fetchBookInfo(finalIsbn);
+              },
+            },
+          ]}
+          onDidDismiss={() => {
+            setIsAlertOpen(false);
+            setTempIsbn(0);
+          }}
+          className={styles.alert}
+        ></IonAlert>
+
         {isLoading ? (
           <LoadSpinner message={`Getting book info...`} />
         ) : (
-          <IonGrid
-            className={`${styles.grid} ${
-              bookInfo === undefined ? styles.centerContent : ''
-            }`}
-          >
-            {bookInfo?.modified && (
-              <>
-                <BookInfo bookInfo={bookInfo} />
-                <IonButton>
-                  Add to Reading List <IonIcon slot="end" icon={book}></IonIcon>
-                </IonButton>
-                <IonButton>
-                  Add to Wish List
-                  <IonIcon slot="end" icon={bookmark}></IonIcon>
-                </IonButton>
-              </>
-            )}
-
-            <IonButton onClick={lerBarcode}>Scan Barcode</IonButton>
-          </IonGrid>
+          <>
+            <IonGrid
+              className={`${styles.grid} ${
+                bookInfo === undefined ? styles.centerContent : ''
+              }`}
+            >
+              {bookInfo?.modified && (
+                <>
+                  <BookInfo bookInfo={bookInfo} detailed={true} />
+                  <IonButton>
+                    Add to Reading List{' '}
+                    <IonIcon slot="end" icon={book}></IonIcon>
+                  </IonButton>
+                  <IonButton>
+                    Add to Wish List
+                    <IonIcon slot="end" icon={bookmark}></IonIcon>
+                  </IonButton>
+                </>
+              )}
+              <IonButton onClick={lerBarcode}>Scan Barcode</IonButton>
+            </IonGrid>
+          </>
         )}
       </IonContent>
     </IonPage>
