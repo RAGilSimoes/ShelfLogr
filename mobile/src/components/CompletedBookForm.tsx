f448d9c19dce49512e6278906cc3d466e18b9775
Ricardo André Simões <ricardo.andre.simoes@gmail.com>
2026-09-21 22:57:36 +0100

    update on completed book form

--- mobile/src/components/CompletedBookForm.tsx
+++ mobile/src/components/CompletedBookForm.tsx
@@ -1,19 +1,39 @@
 import {
-  IonIcon,
-  IonAlert,
-  IonButton,
-  IonSelect,
-  IonSelectOption,
+  IonItem,
+  IonInput,
   IonHeader,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
 } from '@ionic/react';
 
 import styles from './CompletedBookForm.module.css';
 
+import { useState } from 'react';
+
 const CompletedBookForm: React.FC = () => {
+  const maxReviewSize = 300;
+
   return (
     <>
       <form className={styles.loginForm}>
         <IonHeader>Rate This Book</IonHeader>
+        <IonItem>
+          <IonInput
+            label="Review (optional)"
+            maxlength={maxReviewSize}
+            labelPlacement="floating"
+            counter={true}
+          ></IonInput>
+        </IonItem>
+        <IonSegment value="public">
+          <IonSegmentButton value="public">
+            <IonLabel>Public</IonLabel>
+          </IonSegmentButton>
+          <IonSegmentButton value="private">
+            <IonLabel>Private</IonLabel>
+          </IonSegmentButton>
+        </IonSegment>
       </form>
     </>
   );
