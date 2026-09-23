af578978df73845b8d324bd02d194630b32b8a15
Ricardo André Simões <ricardo.andre.simoes@gmail.com>
2026-09-23 09:11:39 +0100

    updated completed book form

--- mobile/src/components/CompletedBookForm.tsx
+++ mobile/src/components/CompletedBookForm.tsx
@@ -5,27 +5,44 @@ import {
   IonSegment,
   IonSegmentButton,
   IonLabel,
+  IonRange,
+  IonIcon,
 } from '@ionic/react';
 
 import styles from './CompletedBookForm.module.css';
+import { star } from 'ionicons/icons';
 
 import { useState } from 'react';
 
 const CompletedBookForm: React.FC = () => {
   const maxReviewSize = 300;
 
+  const [starsRating, setStarsRating] = useState<number>(1);
+
+  const starsDiv = [];
+
+  for (let index = 0; index < starsRating; index++) {
+    starsDiv.push(<IonIcon slot="end" icon={star}></IonIcon>);
+  }
+
   return (
     <>
-      <form className={styles.loginForm}>
-        <IonHeader>Rate This Book</IonHeader>
-        <IonItem>
-          <IonInput
-            label="Review (optional)"
-            maxlength={maxReviewSize}
-            labelPlacement="floating"
-            counter={true}
-          ></IonInput>
-        </IonItem>
+      <form className={styles.completedForm}>
+        <IonHeader className={styles.title}>Rate This Book</IonHeader>
+
+        <div>{starsDiv}</div>
+        <IonRange
+          aria-label="Stars Rating"
+          ticks={true}
+          snaps={true}
+          min={1}
+          max={5}
+          onIonChange={({ detail }) => {
+            setStarsRating(detail.value.valueOf() as number);
+          }}
+          className={styles.range}
+        ></IonRange>
+
         <IonSegment value="public">
           <IonSegmentButton value="public">
             <IonLabel>Public</IonLabel>
@@ -34,6 +51,14 @@ const CompletedBookForm: React.FC = () => {
             <IonLabel>Private</IonLabel>
           </IonSegmentButton>
         </IonSegment>
+        <IonItem className={styles.review}>
+          <IonInput
+            label="Review (optional)"
+            maxlength={maxReviewSize}
+            labelPlacement="floating"
+            counter={true}
+          ></IonInput>
+        </IonItem>
       </form>
     </>
   );
